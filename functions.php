927a170f40299b365eff9afc0e2510dad34c5d0b
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-08-16 13:45:05 +0200

    remove pages, posts and add link to edit client content

--- functions.php
+++ functions.php
@@ -3,3 +3,5 @@ require_once ( get_template_directory() . '/inc/class.Functions.php' );
 require_once ( get_template_directory() . '/inc/class.Walker.php' );
 require_once ( get_template_directory() . '/inc/class.NavbarMenus.php' );
 require_once ( get_template_directory() . '/inc/class.ACFfunctions.php' );
+require_once ( get_template_directory() . '/inc/class.RemoveComments.php' );
+require_once ( get_template_directory() . '/inc/class.RemovePosts.php' );
--- inc/class.RemovePosts.php
+++ inc/class.RemovePosts.php
@@ -1,13 +1,16 @@
 <?php
 class RemovePosts extends Functions {
     public function __construct() {
-        add_action('admin_menu', array ($this, 'disable_posts_admin_menu') );
-        add_action('admin_bar_menu', array($this,  'remove_wp_nodes' ), 666 );
-        add_action('admin_init', array ($this, 'disable_posts_admin_menu_redirect' ) );
- 
+      add_action('admin_menu', array ($this, 'disable_posts_admin_menu') );
+      add_action('admin_bar_menu', array($this,  'remove_wp_nodes' ), 666 );
+      add_action('admin_init', array ($this, 'disable_posts_admin_menu_redirect' ) );
+      add_action( 'admin_menu', array($this, 'linked_url' ) );
+      add_action( 'admin_menu' , array($this, 'linkedurl_function' ) );
+    
     }   
     function disable_posts_admin_menu() {
         remove_menu_page('edit.php');
+        remove_menu_page('edit.php?post_type=page');
     }
     function disable_posts_admin_menu_redirect() {
         global $pagenow;
@@ -18,8 +21,20 @@ class RemovePosts extends Functions {
     function remove_wp_nodes() {
         global $wp_admin_bar;   
         $wp_admin_bar->remove_node( 'new-post' );
+        $wp_admin_bar->remove_node( 'new-page' );
 
         }
+
+    function linked_url() {
+            add_menu_page( 'linked_url', 'Twoja strona', 'read', 'my_slug', '', 'dashicons-text', 1 );
+            }
+
+     function linkedurl_function() {
+          global $menu;
+         $menu[1][2] = get_admin_url()."post.php?post=9&action=edit";
+                }
 }
 new RemovePosts;
+
+
 
