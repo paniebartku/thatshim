eef67f94a88da2635e2bf930c86e7f0a233ee62d
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-08-18 16:46:59 +0200

    small css fixes

--- footer.php
+++ footer.php
@@ -2,17 +2,17 @@
       <div class="footer-main">
           <div class="container">
               <div class="row">
-                  <div class="col-xl-6 col-md-6 col-sm-12 col-12">
+                  <div class="col-xl-4 col-md-4 col-sm-12 col-12">
                   <?php if( is_active_sidebar( 'footer-sidebar-1' ) ){ 
                     dynamic_sidebar( 'footer-sidebar-1' );
                     }else {echo '';}?>
                   </div>
-                  <div class="col-xl-3 col-md-6 col-sm-12 col-12">
+                  <div class="col-xl-4 col-md-4 col-sm-12 col-12">
                       <?php if( is_active_sidebar( 'footer-sidebar-2' ) ){ 
                     dynamic_sidebar( 'footer-sidebar-2' );
                     }else {echo '';}?>
                   </div>
-                  <div class="col-xl-3 col-md-6 col-sm-12 col-12">
+                  <div class="offset-xl-1 col-xl-3 offset-md-1 col-md-3 col-sm-12 col-12">
                       <?php if( is_active_sidebar( 'footer-sidebar-3' ) ){ 
                     dynamic_sidebar( 'footer-sidebar-3' );
                     }else {echo '';}?>
